5a631627931e1e626023b57c62efcbb808107922
Vicente Gonzalez <vgonzalez28@gmail.com>
2018-04-26 13:27:16 -0700

    working run

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,55 @@
+var path = require("path")
+var friends = require("../data/friends.js")
+/*
+Your apiRoutes.js file should contain two routes:
+
+
+
+A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+
+*/
+module.exports = function(app){
+    app.get("/api/friends",function(req,res){
+        res.json(friends)
+    })
+
+    app.post("/api/friends",function(req,res){
+       
+       var bestMatch = {
+           name: "",
+           photo: "",
+           friendDifference : 1000
+       };
+       console.log(req.body);
+       
+       var userData = req.body;
+       var userScores = userData.scores;
+
+       console.log(userScores);
+
+       var totalDifference = 0;
+
+       for (var i = 0; i < friends.length; i++){
+           console.log(friends[i]);
+           totalDifference = 0 ;
+        //    loop through all scores of each friend
+            for( var j = 0; j < friends[i].scores[j]; j++){
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                
+                if (totalDifference <= bestMatch.friendDifference){
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
+});
+
+}
+
+   
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express')
 var app = express()
-var bodyparser = require('body-parser')
+var bodyParser = require('body-parser')
 var path = require('path')
 
 // middleware
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(bodyparser.urlencoded({extended: true}))//allows nested data such as objects and arrays
-app.use(bodyparser.json())
+app.use(bodyParser.urlencoded({extended: true}))//allows nested data such as objects and arrays
+app.use(bodyParser.json({type : 'application/**json'}))
+app.use(bodyParser.raw({type : 'application/vnd.custom-type'}));
+app.use(bodyParser.text({ type : "text/html"}))
 
 
-require('./routing/apiRoutes.js')(app)
 
-require('./routing/htmlRoutes.js')(app)
+require("./app/routing/apiRoutes.js")(app);
+
+require("./app/routing/htmlRoutes.js")(app);
 
 
 
@@ -23,4 +26,5 @@ var PORT = process.env.PORT || 3000
 // listening port
 app.listen(PORT, function (e) {
   if (e) throw e
+  console.log("App listening on port: "+ PORT)
 })
